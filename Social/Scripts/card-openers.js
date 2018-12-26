2d75f9bf6df8251d720de2680fe2b1a847b5446b
nickmly <nickmly@gmail.com>
2018-12-26 17:53:57 -0500

    Login/Register views and routes working

---
+++ Social/Scripts/card-openers.js
@@ -0,0 +1,18 @@
+﻿const cardOpeners = document.getElementsByClassName("card-opener");
+for(var i = 0; i < cardOpeners.length; i ++) {
+    (function(index){
+        cardOpeners[index].addEventListener("click", function() {            
+            const cardBody = document.getElementById("card-body-" + cardOpeners[index].getAttribute("data-id"));
+            var visible = cardBody.style.display == "block"
+            cardOpeners[index].querySelector(".fas").classList.toggle("fa-caret-down");
+            cardOpeners[index].querySelector(".fas").classList.toggle("fa-caret-up");
+            if(visible) {
+                cardBody.style.display = cardBody.style.display = "none";
+                cardOpeners[index].querySelector(".card-opener-text").innerHTML = "View More";
+            } else {
+                cardBody.style.display = "block";
+                cardOpeners[index].querySelector(".card-opener-text").innerHTML = "Close";
+            }
+        });
+    })(i);
+}
---
+++ Social/Scripts/posts.js
@@ -0,0 +1,60 @@
+﻿window.sendAjax = function (url, likeText, dislikeText, otherButton) {
+    var user_id = $(".user-id").attr('id');
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: {
+            user_id: user_id
+        },
+        success: function (data) {
+            var text = data.split(",");
+            console.log(text);
+            likeText.text(text[0]);
+            dislikeText.text(text[1]);
+        }
+    });
+};
+
+window.likePost = function (element) {
+    var post_id = element.find("a").attr('id'); // Post id is stored as the div 
+    post_id = post_id.substring(1, post_id.length);// Remove first letter from ID
+
+    var likeText = element.find("text");
+    var button = element;
+
+    var otherButton = element.siblings().find("span"); // The dislike button
+    var dislikeText = otherButton.find("text");
+
+    var url = "/post/" + post_id + "/like";
+    if (otherButton.hasClass("active"))
+        return dislikePost(otherButton);
+    button.toggleClass("blue").toggleClass("active");
+
+    sendAjax(url, likeText, dislikeText, otherButton);
+};
+
+window.dislikePost = function (element) {
+    var post_id = element.find("a").attr('id'); // Post id is stored as the div 
+    post_id = post_id.substring(1, post_id.length);// Remove first letter from ID
+
+    var dislikeText = element.find("text");
+    var button = element;
+
+    var otherButton = element.siblings().find("span"); // The like button
+    var likeText = otherButton.find("text");
+
+    var url = "/post/" + post_id + "/dislike";
+    if (otherButton.hasClass("active"))
+        return likePost(otherButton);
+    button.toggleClass("red").toggleClass("active");
+
+    sendAjax(url, likeText, dislikeText, otherButton);
+};
+
+$(".likes").on("click", function () {
+    likePost($(this));
+
+});
+$(".dislikes").on("click", function () {
+    dislikePost($(this));
+});
